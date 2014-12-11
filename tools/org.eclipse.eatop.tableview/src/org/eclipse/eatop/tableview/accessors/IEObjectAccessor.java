a69ed45a3a6de59ba7700527d992bba136953937
mist <mikael.stolpe@arccore.com>
2014-12-11 14:57:20 +0100

    Added TableView

---
+++ tools/org.eclipse.eatop.tableview/src/org/eclipse/eatop/tableview/accessors/IEObjectAccessor.java
@@ -0,0 +1,32 @@
+package org.eclipse.eatop.tableview.accessors;
+
+import java.util.Comparator;
+import java.util.List;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.nebula.widgets.nattable.data.IColumnAccessor;
+
+public interface IEObjectAccessor extends IColumnAccessor<EObject> {
+
+	public Object getColumnHeaderValue(int columnIndex);
+	
+	public Object getRowHeaderValue(EObject rowObject);
+	
+	public List<String> getConfigLabels(int columnIndex);
+	
+	public List<?> getComboBoxData(EObject rowObject, int columnIndex);
+	
+	/**
+	 * Convert backing data value to value to be displayed
+	 */
+	public Object canonicalToDisplayValue(Object canonicalValue, int columnIndex);
+
+	/**
+	 * Convert from display value to value in the backing data structure
+	 */
+	public Object displayToCanonicalValue(Object displayValue, int columnIndex);
+	
+	public EObject getHeldData(int columnIndex);
+
+	public Comparator<EObject> getComparator(int columnIndex, int modifier);;
+}
