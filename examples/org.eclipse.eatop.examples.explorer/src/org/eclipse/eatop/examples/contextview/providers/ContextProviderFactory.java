e85541cc16a9fe3f6782abb646da7af72f2d389f
Mikael Stolpe <mikael.stolpe@arccore.com>
2015-04-01 12:06:25 +0200

    Added provider for ASIL levels, shown as list for ports which are referenced by FaultFailure. Fixed NPE bug.

--- examples/org.eclipse.eatop.examples.explorer/src/org/eclipse/eatop/examples/contextview/providers/ContextProviderFactory.java
+++ examples/org.eclipse.eatop.examples.explorer/src/org/eclipse/eatop/examples/contextview/providers/ContextProviderFactory.java
@@ -1,13 +1,16 @@
 package org.eclipse.eatop.examples.contextview.providers;
 
 import org.eclipse.core.runtime.IAdapterFactory;
+import org.eclipse.eatop.eastadl21.FaultFailurePort;
 
 public class ContextProviderFactory implements IAdapterFactory {
 
 	@Override
 	public Object getAdapter(Object adaptableObject, Class adapterType) {
 		if (adapterType == IContextProvider.class) {
-			/* Contribute special context content providers here */
+			if (adaptableObject instanceof FaultFailurePort) {
+				return new ASILLevelContentProvider();
+			}
 		}
 		return null;
 	}
